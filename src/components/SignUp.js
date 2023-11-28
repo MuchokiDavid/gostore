41cb0c35517b4fcdfcb0c9d429dd553d986fd32d
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-11-28 23:12:53 +0300

    Sign up label

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -12,8 +12,6 @@ function SignUp() {
         password:""
     })
     const handleOnChange = (event)=> {
-     
-
         setUser(
              {
                  ...user,
@@ -62,7 +60,7 @@ function SignUp() {
     }
    
 
-    function handleClear(){
+    const handleClear= ()=> {
         pass1Ref.current.value = "";
         pass2Ref.current.value = "";
     }
@@ -121,6 +119,7 @@ function SignUp() {
                 />
               </label>
               <br />
+              <label>
               <input
                 className="block w-full mt-3 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="Password"
@@ -131,6 +130,7 @@ function SignUp() {
                 onChange={handleOnChange}
                 ref={pass1Ref} // Add ref to the input
               />
+              </label>
               <br />
               <label>
                 <input
